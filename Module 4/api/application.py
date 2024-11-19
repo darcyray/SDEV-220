4779af92af3340b10130d991523c022d0746abf6
Darcy Ralstin <fayeralstin@gmail.com>
2024-11-19 00:05:49 -0500

    m04 update

--- Module 4/api/application.py
+++ Module 4/api/application.py
@@ -1,6 +1,14 @@
 from flask import Flask
-
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Book(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(8))
+    description = db.Column(db.String(120))
 
 @app.route('/')
 def index():
@@ -10,3 +18,4 @@ def index():
 def get_books():
     
     return {"books": "book_data"}
+
